aeadb90018f3ff93fee71ad35d4b69cf729bb071
Jeremy Busk <jeremy@pyrofex.net>
2018-03-21 18:01:17 +0000

    test rholang rbls using rosette

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -10,10 +10,27 @@ elif [ -f "build.sbt" ]; then
     cd rosette 
     ./install.sh
     ./build.sh
-    echo "jtest"
-    ls -laht build.out/src/rosette
-    echo "jtest"
+    #ls -laht build.out/src/rosette
     cd ../
+
+    echo "jtest"
+	# Run test RBLs using rosette binary
+	ulimit -s unlimited
+	export ESS_SYSDIR=rosette/rbl/rosette
+	for rbl_file in $(ls rholang/tests/*rbl); do
+		out=$(./rosette/build.out/src/rosette --quiet --boot-dir=rosette/rbl/rosette --boot=boot.rbl ${rbl_file} | grep ^Pass)
+		if [ -z $out ]; then
+			echo "[error] - rbl file ${rbl_file} did not return \"Pass\""
+			exit 1
+		fi
+	done
+    echo "jtest"
+
+#     for rbl_file in $(ls rholang/tests/*rbl); do
+#         #./rosette/build.out/src/rosette --quiet --interactive-repl --boot-dir=rosetteurbl/rosette --boot=boot.rbl ${rbl_file}
+#         ulimit -s unlimited && export ESS_SYSDIR=rosette/rbl/rosette && ./rosette/build.out/src/rosette --quiet --boot-dir=rosette/rbl/rosette --boot=boot.rbl rholang/tests/arithmetic_test.rbl
+#     done
+
     for sub in crypto comm rholang roscala storage node; do
 	(bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
     done
