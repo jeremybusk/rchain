67ee56ae950ac9063ef68bd3675b756eb9276373
Jeremy Busk <jeremy@pyrofex.net>
2018-03-21 15:39:17 +0000

    more changes

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -6,13 +6,14 @@ if [ -d "${SUBPROJECT}" -a -f "${SUBPROJECT}/build.sh" ]; then
     echo "${SUBPROJECT}/build.sh"
     (cd "${SUBPROJECT}"; bash ./build.sh)
 elif [ -f "build.sbt" ]; then
+    sbt -Dsbt.log.noformat=true clean bnfc:generate coverage test coverageReport rpm:packageBin debian:packageBin
     cd rosette 
+    ./install.sh
     ./build.sh
     echo "jtest"
     ls -laht build.out/src/rosette
     echo "jtest"
     cd ../
-    sbt -Dsbt.log.noformat=true clean bnfc:generate coverage test coverageReport rpm:packageBin debian:packageBin
     for sub in crypto comm rholang roscala storage node; do
 	(bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
     done
