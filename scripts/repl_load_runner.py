93423dba2119c3f1a4e2b5b87111b1b06a147911
Jeremy Busk <jeremy@pyrofex.net>
2018-05-10 22:26:33 +0000

    added dockerpy

--- scripts/repl_load_runner.py
+++ scripts/repl_load_runner.py
@@ -2,6 +2,7 @@
 from pexpect import replwrap
 import subprocess
 import argparse
+import docker
 import sys
 
 parser = argparse.ArgumentParser()
@@ -13,9 +14,11 @@ parser.add_argument("-m", "--memory", dest='memory', type=int, default=1024, hel
 parser.add_argument("-n", "--network", dest='network', type=str, default="testnet1.rchain", help="set docker memory for repl client node")
 args = parser.parse_args()
 
-result = subprocess.run(["docker", "volume", "create"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-volume_name = result.stdout.decode('ascii').rstrip('\r\n')
-print("creating tmp volume: {}".format(volume_name))
+#result = subprocess.run(["docker", "volume", "create"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#volume_name = result.stdout.decode('ascii').rstrip('\r\n')
+#print("creating tmp volume: {}".format(volume_name))
+client = docker.from_env()
+volume = client.volumes.create()
 
 # Delete rnode-repl on network if it exists
 # result = subprocess.run(["docker", "container", "ls", "--all", "--format", "{{.Names}}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -25,8 +28,9 @@ print("creating tmp volume: {}".format(volume_name))
 #     print("removing {}".format(container))
 #     result = subprocess.run([ "docker", "container", "rm", "-f", container ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+subprocess.call(["docker", "container", "rm", "-f", "rnode-repl.{}".format(args.network)])
 # Run rnode-repl loader with replwrap
-cmd = "sudo docker run --rm -it -v {}:/var/lib/rnode --cpus=.5 --memory=1024m --name rnode-repl.{} --network {} coop.rchain/rnode --grpc-host node0.testnet1.rchain -r".format(volume_name, args.network, args.network)
+cmd = "sudo docker run --rm -it -v {}:/var/lib/rnode --cpus=.5 --memory=1024m --name rnode-repl.{} --network {} coop.rchain/rnode --grpc-host node0.testnet1.rchain -r".format(volume.name, args.network, args.network)
 repl_cmds = ['5', '@"stdout"!("foo")']
 conn = replwrap.REPLWrapper(cmd, args.prompt, None)
 for i in range(args.repeat):
@@ -35,5 +39,9 @@ for i in range(args.repeat):
     print("repetition: {} output: {}".format(i, result))
 
 # Clean up
-subprocess.run(["docker", "container", "rm", "-f", "rnode-repl.{}".format(args.network)])
-result = subprocess.run(["docker", "volume", "rm", volume_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#subprocess.run(["docker", "container", "rm", "-f", "rnode-repl.{}".format(args.network)])
+#result = subprocess.run(["docker", "volume", "rm", volume_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+subprocess.call(["docker", "container", "rm", "-f", "rnode-repl.{}".format(args.network)])
+# client.containers.stop("rnode-repl.{}".format(args.network))
+# client.containers.prune("rnode-repl.{}".format(args.network))
+client.volumes.prune()
