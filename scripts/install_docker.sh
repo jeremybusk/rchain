735de0a788262d8ca626df53e14226c14111f1b4
Jeremy Busk <jeremy@pyrofex.net>
2018-05-12 12:21:00 +0000

    try

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -3,6 +3,9 @@ export PATH=$PATH:$(pwd -P)/scripts
 source header.sh
 
 sudo curl -sSL https://get.docker.com/ | sh
-sleep 10  # wait for docker to start
 ps -eaf | grep docker
+journalctl -a
+service docker start
+systemctl start docker
+sleep 10  # wait for docker to start
 sudo docker info
