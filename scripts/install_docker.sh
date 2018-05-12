a74fa273faca04860a7d781209c804dd1c04f6dd
Jeremy Busk <jeremy@pyrofex.net>
2018-05-12 13:27:39 +0000

    longshot3

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -3,11 +3,12 @@ export PATH=$PATH:$(pwd -P)/scripts
 source header.sh
 
 sudo curl -sSL https://get.docker.com/ | sh
+sleep 10  # wait for docker to start
 ps -eaf | grep docker
 journalctl -a
 service docker start
 ps -eaf | grep docker
-systemctl start docker
+#systemctl start docker
 ps -eaf | grep docker
 sleep 10  # wait for docker to start
 sudo docker info
