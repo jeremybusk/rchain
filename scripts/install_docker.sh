9f5a6ea6d78cebf0d6c10caf014878cff13c7534
Jeremy Busk <jeremy@pyrofex.net>
2018-05-12 13:00:44 +0000

    try docker dind

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -6,6 +6,8 @@ sudo curl -sSL https://get.docker.com/ | sh
 ps -eaf | grep docker
 journalctl -a
 service docker start
+ps -eaf | grep docker
 systemctl start docker
+ps -eaf | grep docker
 sleep 10  # wait for docker to start
 sudo docker info
