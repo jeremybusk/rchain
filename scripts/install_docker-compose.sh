28badfc1cc865aac84acf700fa163e4bf3bf6a85
Jeremy Busk <jeremy@pyrofex.net>
2018-05-12 11:45:54 +0000

    install sudo

--- scripts/install_docker-compose.sh
+++ scripts/install_docker-compose.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+apt-get install sudo -yq
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+apt-get install sudo -yq
 sudo curl -sSL https://get.docker.com/ | sh
 sudo docker info
